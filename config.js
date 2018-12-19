5af30a970691d5595bb4a9fa77b60cf86fa04a11
Song <songxinxin01@baidu.com>
2018-12-19 15:45:59 +0800

    'chushihua'

---
+++ config.js
@@ -0,0 +1,14 @@
+const config = {
+
+  port: 3001,
+
+  database: {
+    DATABASE: 'koa_demo',
+    USERNAME: 'test',
+    PASSWORD: '123456',
+    PORT: '3306',
+    HOST: '47.93.253.162'
+  }
+}
+
+module.exports = config
